7874df9289b2ca81bcca549b8d6a9e04a1557016
Marshall Ku <marshall@kakao.com>
2022-08-20 01:18:01 +0900

    Add configuration for rollup

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import { babel } from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import pkg from "./package.json";
+
+const makeExternalPredicate = (externalArr) => {
+    if (externalArr.length === 0) {
+        return () => false;
+    }
+    const pattern = new RegExp(`^(${externalArr.join("|")})($|/)`);
+    return (id) => pattern.test(id);
+};
+
+const basePlugins = [
+    babel({
+        babelHelpers: "bundled",
+        plugins: ["transform-object-rest-spread"],
+    }),
+    typescript(),
+];
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            { file: pkg.main, format: "cjs" },
+            { file: pkg.module, format: "es" },
+        ],
+        external: makeExternalPredicate([
+            ...Object.keys(pkg.dependencies || {}),
+            ...Object.keys(pkg.peerDependencies || {}),
+        ]),
+        plugins: [...basePlugins],
+    },
+    {
+        input: "src/index.ts",
+        output: {
+            file: pkg.types,
+            name: "PostScribe",
+        },
+        plugins: [dts()],
+    },
+];
