28f8429243e5d640e3930bde05d9c4e8d4ffa943
Marshall Ku <marshall@kakao.com>
2022-08-20 10:34:14 +0900

    Fix return type for callbacks in postscribe

--- src/@types/postscribe.d.ts
+++ src/@types/postscribe.d.ts
@@ -1,25 +1,25 @@
 declare module "postscribe" {
     export interface PostScribeOption {
         /** A callback called when an async script has loaded */
-        afterAsync?: () => any;
+        afterAsync?: () => void;
         /** A callback called immediately before removing from the write queue */
-        afterDequeue?: () => any;
+        afterDequeue?: () => void;
         /** A callback called sync after a stream's first thread release */
-        afterStreamStart?: () => any;
+        afterStreamStart?: () => void;
         /** A callback called after writing buffered document.write calls */
-        afterWrite?: () => any;
-        /** callback that will be called when writing is finished */
-        done?: () => any;
+        afterWrite?: () => void;
+        /** A callback that will be called when writing is finished */
+        done?: () => void;
         /** A boolean that allows disabling the autoFix feature of prescribe */
         autoFix?: boolean;
         /** A callback called immediately before adding to the write queue */
-        beforeEnqueue?: () => any;
+        beforeEnqueue?: () => void;
         /** A callback called before writing buffered document.write calls */
-        beforeWrite?: () => any;
+        beforeWrite?: () => void;
         /** A callback called before writing a token */
-        beforeWriteToken?: () => any;
+        beforeWriteToken?: () => void;
         /** A function that throws the error by default, but could be overwritten */
-        error?: () => any;
+        error?: () => void;
         /** A boolean whether to let scripts w/ async attribute set fall out of the queue */
         releaseAsync?: boolean;
     }
